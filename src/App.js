80c0f607f567fb43373bee2d8601899168568648
v-wang <victorwang920@gmail.com>
2022-01-11 19:58:01 -0500

    basic setup and started top section of intro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import profilePic from './assets/profile.jpeg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-holder'>
+      <div className='bg-inner-one'>
+        <div className='bg-inner-two'>
+          <div className='bg-main'>
+            <nav>
+              <a href=''>About</a>
+              <a href=''>Projects</a>
+              <a href=''>Learning</a>
+              <a href=''>Contact</a>
+            </nav>
+            <div className='intro-top'>
+              <div className='intro-text-area'>
+                <h2>Hi, I'm Victor, Software Developer.</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam.
+                </p>
+                <button>Download Resume</button>
+              </div>
+              <div className='intro-profile-pic'>
+                <img src={profilePic} alt='victor profile pic' />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
