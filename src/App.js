e2e8a8974b6d47e321fd2cd84aad45158c4804c2
v-wang <victorwang920@gmail.com>
2022-01-11 20:36:07 -0500

    added about, projeects, contact, learning components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
-import profilePic from './assets/profile.jpeg';
+import profilePic from './assets/profile-pic.png';
+import About from './components/About';
+import Projects from './components/Projects';
+import Learning from './components/Learning';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -21,12 +25,18 @@ function App() {
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                   Ut enim ad minim veniam.
                 </p>
-                <button>Download Resume</button>
+                <button>
+                  <h3>Download Resume</h3>
+                </button>
               </div>
               <div className='intro-profile-pic'>
                 <img src={profilePic} alt='victor profile pic' />
               </div>
             </div>
+            <About></About>
+            <Projects></Projects>
+            <Learning></Learning>
+            <Contact></Contact>
           </div>
         </div>
       </div>
---
+++ src/components/Projects.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Projects(props) {
+  return <div></div>;
+}
+
+export default Projects;
