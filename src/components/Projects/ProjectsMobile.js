68111f1ae99d1a02eda75f9a28958fba87d22ce3
v-wang <victorwang920@gmail.com>
2022-02-03 22:22:10 -0500

    styled filter buttons

--- src/components/Projects/ProjectsMobile.js
+++ src/components/Projects/ProjectsMobile.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import './projectsmobile.css';
+import projects from '../../assets/data/projects.json';
+import { useState } from 'react';
 
 function ProjectsMobile(props) {
-  return <div></div>;
+  const [filteredProjects, setFilteredProjects] = useState([]);
+
+  const categories = new Set(
+    projects
+      .map((project) => {
+        return project.categories;
+      })
+      .flat()
+  );
+
+  return (
+    <div className='projects-container'>
+      <h1>Project</h1>
+      <div className='projects-filter'>
+        {Array.from(categories)
+          .sort()
+          .map((category) => {
+            return (
+              <div className='category' key={`${category} button`}>
+                {category}
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
 }
 
 export default ProjectsMobile;
