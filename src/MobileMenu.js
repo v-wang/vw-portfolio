539f3d4b7d5ac82d0db89596992d138153ca66d7
v-wang <victorwang920@gmail.com>
2022-02-09 18:57:01 -0500

    fix menu close

--- src/MobileMenu.js
+++ src/MobileMenu.js
@@ -2,25 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function MobileMenu() {
-  function closeNav(ev) {
-    ev.target.parentElement.parentElement.style.width = '0';
+  function closeNav() {
+    const menu = document.querySelector('.mobile-menu');
+    menu.style.width = '0';
   }
   return (
     <div className='mobile-menu'>
       <nav id='mobile-nav'>
-        <a href={null} className='closebtn' onClick={(ev) => closeNav(ev)}>
+        <a href={null} className='closebtn' onClick={closeNav}>
           &times;
         </a>
-        <Link to='/about'>
+        <Link to='/about' onClick={closeNav}>
           <button>About</button>
         </Link>
-        <Link to='/'>
+        <Link to='/' onClick={closeNav}>
           <button>Projects</button>
         </Link>
-        <Link to='/learning'>
+        <Link to='/learning' onClick={closeNav}>
           <button>Learning</button>
         </Link>
-        <Link to='/contact'>
+        <Link to='/contact' onClick={closeNav}>
           <button>Contact</button>
         </Link>
       </nav>
