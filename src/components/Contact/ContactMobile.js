a738d82eac27185f2e57210520f5b1e16f55d4c3
v-wang <victorwang920@gmail.com>
2022-02-09 15:36:11 -0500

    restyled mobile learning page

--- src/components/Contact/ContactMobile.js
+++ src/components/Contact/ContactMobile.js
@@ -1,7 +1,48 @@
-import React from 'react';
-
+import './contactmobile.css';
 function ContactMobile(props) {
-  return <div></div>;
+  return (
+    <div className='contact-holder-mobile'>
+      <h2>Contact</h2>
+      <div className='contact-content-mobile'>
+        <div className='contact-text-mobile'>
+          <p>
+            I’m currently located in Philadelphia, PA. I’m open to both
+            relocation and remote work. If you have something else in mind, or
+            just want to say hi, shoot me a message! I appreciate you taking the
+            time to stop by.&nbsp;
+            <em>
+              As a small thanks, check out some of my favorite spots to eat in
+              Philly!
+            </em>
+          </p>
+        </div>
+        <div className='contact-links-holder-mobile'>
+          <div className='contact-link-mobile'>
+            <img src={require('../../assets/images/email.png')} />
+            <a href='mailto: mailbox.vmw@gmail.com'>mailbox.vmw@gmail.com</a>
+          </div>
+          <div className='contact-link-mobile'>
+            <img src={require('../../assets/images/github.png')} />
+            <a href='https://github.com/v-wang' target='_blank'>
+              github.com/v-wang
+            </a>
+          </div>
+          <div className='contact-link-mobile'>
+            <img src={require('../../assets/images/linkedin.png')} />
+            <a href='https://www.linkedin.com/in/vicw/' target='_blank'>
+              linkedin.com/in/vicw/
+            </a>
+          </div>
+        </div>
+      </div>
+      <iframe
+        src='https://www.google.com/maps/d/u/1/embed?mid=1Y1uOVtF2Kb3FvgoN_UPy5CoT3uBkukxn&ehbc=2E312F'
+        width='100%'
+        height='480'
+        id='map-mobile'
+      ></iframe>
+    </div>
+  );
 }
 
 export default ContactMobile;
--- src/components/Learning/LearningMobile.js
+++ src/components/Learning/LearningMobile.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import './learningmobile.css';
+import learningSubs from '../../assets/data/learning.json';
 
 function LearningMobile(props) {
-  return <div></div>;
+  return (
+    <div>
+      <div className='learning-holder-mobile'>
+        <h1>Learning</h1>
+        <div className='learning-sub-holder-mobile'>
+          {learningSubs.map((sub) => {
+            return (
+              <div className='learning-sub-mobile'>
+                <img src={sub.img_url} alt={sub.title} />
+                <h3>{sub.title}</h3>
+                <p>{sub.body}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LearningMobile;
