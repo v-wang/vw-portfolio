db0c786cf6d5d21762cf27e3320d397f0943e8f2
v-wang <victorwang920@gmail.com>
2022-02-09 19:26:45 -0500

    fixed mobile element alignments

--- src/components/Learning/LearningMobile.js
+++ src/components/Learning/LearningMobile.js
@@ -8,23 +8,21 @@ function LearningMobile(props) {
     scroll.scrollToTop();
   }
   return (
-    <div>
-      <div className='learning-holder-mobile'>
-        <button onClick={goUp} className='up-arrow-btn'>
-          <img src={upArrow} className='up-arrow' />
-        </button>
-        <h1>Learning</h1>
-        <div className='learning-sub-holder-mobile'>
-          {learningSubs.map((sub) => {
-            return (
-              <div className='learning-sub-mobile'>
-                <img src={sub.img_url} alt={sub.title} />
-                <h3>{sub.title}</h3>
-                <p>{sub.body}</p>
-              </div>
-            );
-          })}
-        </div>
+    <div className='learning-holder-mobile'>
+      <button onClick={goUp} className='up-arrow-btn'>
+        <img src={upArrow} className='up-arrow' />
+      </button>
+      <h1>Learning</h1>
+      <div className='learning-sub-holder-mobile'>
+        {learningSubs.map((sub) => {
+          return (
+            <div className='learning-sub-mobile'>
+              <img src={sub.img_url} alt={sub.title} />
+              <h3>{sub.title}</h3>
+              <p>{sub.body}</p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
